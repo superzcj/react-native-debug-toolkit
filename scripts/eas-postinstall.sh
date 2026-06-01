2cb803999cb0749ce1b48f017ab9e30400864d1c
zcj <405911774@qq.com>
2026-06-01 10:44:38 +0800

    refactor: remove old embed command path

--- scripts/eas-postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# react-native-debug-toolkit: EAS Build postInstall hook
-# Usage in eas.json:
-#   "postInstall": "bash node_modules/react-native-debug-toolkit/scripts/eas-postinstall.sh"
-set -e
-npx debug-toolkit embed --yes
